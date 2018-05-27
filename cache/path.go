9411adbcd37663ce1657aad4dd48d164fffa2bd3
Glen Nakamura <glen@imodulo.com>
2018-05-27 11:28:08 -1000

    cache: replace basedir with config.DocumentRoot

--- cache/path.go
+++ cache/path.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"yumcache/config"
 )
 
 const centos = `^.*(?i:centos).*(/[1-9](?:\.?[0-9])*/[a-z]+/x86_64/.*$)`
@@ -15,16 +16,14 @@ var mirrors = map[string]*regexp.Regexp{
 	"epel":   regexp.MustCompile(epel),
 }
 
-var basedir = "/var/cache/yumcache"
-
 func localPath(req *http.Request) string {
 	url := path.Join(req.Host, path.Clean(req.URL.Path))
 	for dist, re := range mirrors {
 		if match := re.FindStringSubmatch(url); match != nil {
-			return path.Join(basedir, dist, match[1])
+			return path.Join(config.DocumentRoot, dist, match[1])
 		}
 	}
-	return path.Join(basedir, url)
+	return path.Join(config.DocumentRoot, url)
 }
 
 func createPath(name string) (*os.File, error) {

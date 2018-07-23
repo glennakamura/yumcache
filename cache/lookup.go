91c21db01227a6607d973677a5bc2ead2615d489
Glen Nakamura <glen@imodulo.com>
2018-07-22 20:18:15 -1000

    cache: only cache complete files

--- cache/lookup.go
+++ cache/lookup.go
@@ -11,15 +11,19 @@ var mutex sync.Mutex
 
 func Lookup(req *http.Request) http.Handler {
 	path := localPath(req)
+	isCompleteFile := req.Header.Get("Range") == ""
 	mutex.Lock()
 	defer mutex.Unlock()
 	if dl, ok := downloads[path]; ok {
-		return concurrentHandler(path, dl)
+		if isCompleteFile {
+			return concurrentHandler(path, dl)
+		}
+		return proxyHandler
 	}
 	if stat, err := os.Stat(path); err == nil && !stat.IsDir() {
 		return fileHandler(path)
 	}
-	if path[len(path)-4:] == ".rpm" {
+	if isCompleteFile && path[len(path)-4:] == ".rpm" {
 		return downloadHandler(path)
 	}
 	return proxyHandler
